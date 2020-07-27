6a9bcb3fe2ec27ea7942aa59f69ca19e0e5ade03
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-07-28 00:01:56 +0200

    set up pre commit hook

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import React from "react"
 import PropTypes from "prop-types"
 
 import Header from "../Header"
@@ -9,9 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main sx={{ paddingTop: 50 }}>
-        {children}
-      </main>
+      <main sx={{ paddingTop: 50 }}>{children}</main>
       <footer />
     </>
   )
