f685ab6c2165adf1d1425a4773ff01dc2019c737
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-08-03 03:05:13 +0200

    hotfix: wrap 404 page with basket provider

--- src/data/products/great_dummy.md
+++ src/data/products/great_dummy.md
@@ -4,11 +4,10 @@ slug: great-dummy
 price: 104.99
 excerpt: Something that you trully need, but haven’t know about it yet
 image: ../../images/products/dummy.svg
-description: 
-    Something that you trully need, but haven’t know about it yet. Multiple winner of Community Awarads.
+description: Something that you trully need, but haven’t know about it yet. Multiple winner of Community Awarads.
 Awards:
-* Award Local 2020
-* Award Global 2019 
-* Best Value Award
+  - Award Local 2020
+  - Award Global 2019
+  - Best Value Award
 tag: popular
 ---
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,16 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import { BasketProvider } from "../components/Basket"
 
 export default function NotFoundPage() {
   return (
-    <Layout>
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
+    <BasketProvider>
+      <Layout>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Layout>
+    </BasketProvider>
   )
 }
